2f7a01583150a5eaa14e2b6652e184534652f4f1
Robert Evanik <rob.evanik@gmail.com>
2021-11-06 10:05:37 -0400

    Added validation to user input

--- assests/script.js
+++ assests/script.js
@@ -1,11 +1,58 @@
 // Assignment code here
-var generatePassword = function () {
+var getLength = function () {
+  //debugger;
   var length = window.prompt("How many characters would you like your password to contain? Please choose a number between 8 and 128.");
-  var isLowercase = window.confirm("Would you like your password to include: Lowercase characters?");
+  length = parseInt(length);
+  if (length < 8 || length > 128) {
+    window.alert("Your password can only be between 8 and 128 characters, please submit again");
+    getLength();
+  }
+  return length;
+}
+
+// Get Random Characters
+var getRandoLower = function () {
+  var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const randoLower = lowerCaseChar[Math.floor(Math.random() * lowerCaseChar.length)];
+  return randoLower;
+}
+
+var getRandoUpper = function () {
+  var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const randoUpper = upperCaseChar[Math.floor(Math.random() * upperCaseChar.length)];
+  return randoUpper;
+}
+
+var getRandoNum = function () {
+  const randonum = Math.floor(Math.random() * 10);
+  return randonum;
+}
+
+var getRandoSpecial = function () {
+  var specialChar = ["!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}"];
+  const randoSpecial = specialChar[Math.floor(Math.random() * specialChar.length)];
+  return randoSpecial;
+}
+
+
+
+var generatePassword = function () {
+  var pwLength = getLength();
+  var isLowercase = window.confirm("Would you like your password to include: LowerCase characters?");
   var isUppercase = window.confirm("Would you like your password to include: Uppercase characters?");
   var isNumeric = window.confirm("Would you like your password to include: Numeric characters?");
   var isSpecial = window.confirm("Would you like your password to include: Special characters?");
-  return;
+
+
+
+  for (i = 0; i < pwLength; i++) {
+    var password = "";
+
+    password.concat();
+  }
+
+
+  console.log(pwLength, isLowercase, isUppercase, isNumeric, isSpecial);
 }
 
 // Get references to the #generate element
@@ -22,4 +69,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
 
